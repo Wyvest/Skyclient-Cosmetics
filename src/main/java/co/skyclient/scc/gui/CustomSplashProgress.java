5c214c2c74e401c1e2c8834dc822e663f494b84b
ev chang <wyvestbusiness@gmail.com>
2024-05-22 00:46:04 -0400

    Update CustomSplashProgress.java

--- src/main/java/co/skyclient/scc/gui/CustomSplashProgress.java
+++ src/main/java/co/skyclient/scc/gui/CustomSplashProgress.java
@@ -186,7 +186,7 @@ public class CustomSplashProgress {
 
         Multithreading.runAsync(() -> {
             try {
-                String text = NetworkUtils.getString("https://cdn.jsdelivr.net/gh/KTibow/Skyclient@main/docs/assets/funfacts.txt");
+                String text = NetworkUtils.getString("https://cdn.jsdelivr.net/gh/SkyblockClient/Website@main/docs/assets/funfacts.txt");
                 if (text != null) {
                     String[] lines = text.split("\n");
                     int index = (int) (Math.random() * lines.length);
